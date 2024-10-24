e214dbb27b6399b1222bcd80e244d5171a4f6da0
Albina Akhmatova <akhmatova.albino4ka@yandex.ru>
2024-10-25 00:24:30 +0500

    /start message from bot + start keyboard

--- src/src/main/java/org/example/Bot.java
+++ src/src/main/java/org/example/Bot.java
@@ -2,9 +2,21 @@ package org.example;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.List;
+
 public class Bot extends TelegramLongPollingBot {
+    var page1 = InlineKeyboardButton.builder().text("Calculate natal chart").callbackData("page1").build();
+    var page2 = InlineKeyboardButton.builder().text("Read more...").callbackData("page2").build();
+    var page3 = InlineKeyboardButton.builder().text("Display aura color").callbackData("page3").build();
+
+    private InlineKeyboardMarkup keyboardM1;
+    private InlineKeyboardMarkup keyboardM2;
+    private InlineKeyboardMarkup keyboardM3;
 
     @Override
     public String getBotUsername() {
@@ -37,5 +49,7 @@ public class Bot extends TelegramLongPollingBot {
             throw new RuntimeException(e);
         }
     }
+
+
 
 }
